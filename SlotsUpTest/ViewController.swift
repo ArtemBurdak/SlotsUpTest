f9eb31d903bd83fc1a7bd9d702a7c9b23164c421
ArtemBurdak <artemboordak@gmail.com>
2019-06-25 16:41:37 +0300

    Initial Commit

---
+++ SlotsUpTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlotsUpTest
+//
+//  Created by Artem on 6/25/19.
+//  Copyright © 2019 Artem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
